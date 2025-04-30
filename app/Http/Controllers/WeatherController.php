89465e8bed347757821c725f85d8f389bec7ec22
Dennis Muturi <deynohmuturi@gmail.com>
2025-04-30 21:01:58 +0300

    Add WeatherController to fetch weather details from OpenWeather API

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+
+
+class WeatherController extends Controller
+{
+    //
+
+    public function fetchWeatherDetails(Request $request): JsonResponse
+    {
+        $validatedData = $request->validate([
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric',
+            'units' => 'required|string|in:standard,metric,imperial',
+        ]);
+        $apiKey=config('services.openweather.key');
+        $response = Http::get("https://api.openweathermap.org/data/3.0/onecall",[
+            'lat'=>$validatedData['latitude'],
+            'lon'=>$validatedData['longitude'],
+            'units'=>$validatedData['units'],
+            'exclude'=>'minutely,hourly,alerts',
+            'appid'=>$apiKey
+        ]);
+        $data = $response->json();
+        return response()->json($data);
+    }
+}
